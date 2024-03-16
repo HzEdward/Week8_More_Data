26514393179933195da8ae5f6ad0af048da3b14b
QirenDong <edwardqirendong@163.com>
2024-03-16 15:00:42 +0000

    change_name

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 # execute main function in pickwhitelist.py for 100 times
 
-import pick_whitelist
+import only_whitelist
 
 def main():
     count = 0
     for i in range(50):
         count += 1
-        pick_whitelist.main()
+        only_whitelist.main()
 
     print(count)
 
